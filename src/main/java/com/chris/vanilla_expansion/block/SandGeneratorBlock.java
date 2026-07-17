8c2f5b7173b15aee478df85827d6048e0e1daf09
Eagledon4364 <christiaandiaanvanjaarsveld@gmail.com>
2026-07-17 19:40:46 +0200

    111111

--- src/main/java/com/chris/vanilla_expansion/block/SandGeneratorBlock.java
+++ src/main/java/com/chris/vanilla_expansion/block/SandGeneratorBlock.java
@@ -1,22 +1,46 @@
 package com.chris.vanilla_expansion.block;
 
 import com.chris.vanilla_expansion.block.entity.SandGeneratorBlockEntity;
+import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
 import org.jspecify.annotations.Nullable;
 
-public class SandGeneratorBlock  extends Block implements EntityBlock {
+public class SandGeneratorBlock extends BaseEntityBlock {
 
     public SandGeneratorBlock(Properties properties) {
         super(properties);
     }
 
     @Override
+    protected MapCodec<? extends BaseEntityBlock> codec() {
+        return simpleCodec(SandGeneratorBlock::new);
+    }
+
+    @Override
     public @Nullable BlockEntity newBlockEntity(@NotNull BlockPos worldPosition, @NotNull BlockState blockState) {
         return new SandGeneratorBlockEntity(worldPosition, blockState);
+    }
+    @Override
+    protected InteractionResult useItemOn(ItemStack stack, BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+        if (!(world.getBlockEntity(pos) instanceof SandGeneratorBlockEntity sandGeneratorBlockEntity)) {
+            return InteractionResult.PASS;
+        }
+
+        if (!player.getItemInHand(hand).isEmpty() && sandGeneratorBlockEntity.isEmpty()) {
+            sandGeneratorBlockEntity.setItem(0, player.getItemInHand(hand).copy());
+            player.getItemInHand(hand).setCount(0);
+        }
+
+        return InteractionResult.SUCCESS;
     }
 }
--- src/main/java/com/chris/vanilla_expansion/block/entity/SandGeneratorBlockEntity.java
+++ src/main/java/com/chris/vanilla_expansion/block/entity/SandGeneratorBlockEntity.java
@@ -2,32 +2,52 @@ package com.chris.vanilla_expansion.block.entity;
 
 import com.chris.vanilla_expansion.block.ModBlockEntities;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.NonNullList;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.MenuProvider;
-import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.ContainerHelper;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import org.jspecify.annotations.Nullable;
+import net.minecraft.world.level.storage.ValueInput;
+import net.minecraft.world.level.storage.ValueOutput;
+import org.jetbrains.annotations.NotNull;
 
-public class SandGeneratorBlockEntity extends BlockEntity implements MenuProvider {
-    private final NonNullList<ItemStack> items = NonNullList.withSize(2, ItemStack.EMPTY);
+public class SandGeneratorBlockEntity extends BlockEntity implements ImplementedContainer {
+    private final NonNullList<@NotNull ItemStack> items = NonNullList.withSize(1, ItemStack.EMPTY);
+    private int timeSinceDropped = 0;
 
     public SandGeneratorBlockEntity(BlockPos worldPosition, BlockState blockState) {
         super(ModBlockEntities.SANDGENERATOR_BE, worldPosition, blockState);
     }
 
+    public static void tick(Level world, BlockPos blockPos, BlockState blockState, SandGeneratorBlockEntity sandGeneratorBlockEntity) {
+        if (sandGeneratorBlockEntity.isEmpty()) return;
+        sandGeneratorBlockEntity.timeSinceDropped++;
 
+        if (sandGeneratorBlockEntity.timeSinceDropped < 10) return;
+        sandGeneratorBlockEntity.timeSinceDropped = 0;
+
+        ItemStack duplicate = sandGeneratorBlockEntity.getItem(0).split(1);
+
+        Block.popResourceFromFace(world, blockPos, Direction.UP, duplicate);
+        Block.popResourceFromFace(world, blockPos, Direction.UP, duplicate);
+    }
     @Override
-    public Component getDisplayName() {
-        return null;
+    public NonNullList<ItemStack> getItems() {
+        return this.items;
+    }
+    @Override
+    protected void loadAdditional(ValueInput input) {
+        super.loadAdditional(input);
+        ContainerHelper.loadAllItems(input, this.items);
     }
 
     @Override
-    public @Nullable AbstractContainerMenu createMenu(int containerId, Inventory inventory, Player player) {
-        return null;
+    protected void saveAdditional(ValueOutput output) {
+        ContainerHelper.saveAllItems(output, this.items);
+        super.saveAdditional(output);
     }
+
 }
